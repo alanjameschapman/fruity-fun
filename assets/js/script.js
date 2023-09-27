a448101f09dde83a3a59b4cb5824a8c629c12f7f
alanjameschapman <>
2023-09-27 17:33:02 +0000

    create 4 random numbers and assign them to 4 fruits

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,8 +14,16 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 });
-
+/**
+ * Creates 4 random integers between 1 and 7 and assigns them to 4 fruits.
+ * fruit3 and fruit4 will only be used in medium and Fruity! games respectively.
+ * Assigning these fruits 
+ */
 function runGame() {
+    let fruit1 = Math.floor(Math.random() * 7) + 1;
+    let fruit2 = Math.floor(Math.random() * 7) + 1;
+    let fruit3 = Math.floor(Math.random() * 7) + 1;
+    let fruit4 = Math.floor(Math.random() * 7) + 1;
 
 }
 
