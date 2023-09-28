fe7126a87370978b1b36901bb5f4624b5a93f656
alanjameschapman <>
2023-09-28 06:18:51 +0000

    initial code for displayEasyQuestion

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,21 +10,28 @@ document.addEventListener('DOMContentLoaded', function () {
             } else {
                 let gameMode = this.getAttribute('data-type');
                 alert(`you clicked ${gameMode}`);
+                runGame(gameMode);
             }
         });
     }
+    runGame('easy');
 });
 /**
  * Creates 4 random integers between 1 and 7 and assigns them to 4 fruits.
- * fruit3 and fruit4 will only be used in medium and Fruity! games respectively.
- * Assigning these fruits 
+ * strawberry and pear will only be used in medium and Fruity! games respectively.
  */
-function runGame() {
-    let fruit1 = Math.floor(Math.random() * 7) + 1;
-    let fruit2 = Math.floor(Math.random() * 7) + 1;
-    let fruit3 = Math.floor(Math.random() * 7) + 1;
-    let fruit4 = Math.floor(Math.random() * 7) + 1;
+function runGame(gameMode) {
+    let banana = Math.floor(Math.random() * 7) + 1;
+    let orange = Math.floor(Math.random() * 7) + 1;
+    let strawberry = Math.floor(Math.random() * 7) + 1;
+    let pear = Math.floor(Math.random() * 7) + 1;
 
+    if (gameMode === 'easy') {
+        displayEasyQuestion(banana, orange);
+    } else {
+        alert(`unknown gameMode ${gameMode}`);
+        throw `unknown gameMode ${gameMode}. Abort mission!`;
+    }
 }
 
 function calculateCorrectAnswer() {
@@ -44,6 +51,16 @@ function incrementIncorrect() {
 }
 
 function displayEasyQuestion() {
+    let banana = Math.floor(Math.random() * 7) + 1;
+    let orange = Math.floor(Math.random() * 7) + 1;
+    let bananaMultiple = Math.floor(Math.random() * 3) + 1;
+    let orangeMultiple = Math.floor(Math.random() * 3) + 1;
+    console.log(banana);
+    console.log(orange);
+
+    let easyHtml = document.getElementsByClassName('question-area')[0];
+    easyHtml.innerHTML = `<div><span>${bananaMultiple} </span> + <span>${orangeMultiple}</span> = <span>${banana * bananaMultiple + orange * orangeMultiple}</span></div>
+    <div><span>${bananaMultiple} </span> - <span>${orangeMultiple}</span> = <span>${banana * bananaMultiple - orange * orangeMultiple}</span></div>`;
 
 }
 
@@ -53,4 +70,4 @@ function displayMediumQuestion() {
 
 function displayHardQuestion() {
 
-}
+};;
